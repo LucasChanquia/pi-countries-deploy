ca86a02502582a7e4d805257d04fcde0cdeccfc7
LucasChanquia <lucaschanquia@gmail.com>
2023-06-23 00:56:08 -0300

    modify

--- server/index.js
+++ server/index.js
@@ -6,33 +6,8 @@ require('dotenv').config();
 const {PORT} = process.env
 
 conn.sync({ force: false }).then(() => {
+  console.log(PORT);
   server.listen(PORT, async () => {
-    
-    // const dbCountries = Country.findAll()
-
-    // if(!dbCountries.length){
-    //   const urlApi = await axios.get('http://localhost:5000/countries')
-    //   const infoApi = await urlApi.data.map((e)=>{
-    //     return {
-    //       id: e.cca3,
-    //       name: e.name.common,
-    //       image: e.flags.svg,
-    //       continent: e.continents[0],
-    //       capital: e.capital ? e.capital[0] : 'Not Found',
-    //       subregion: e.subregion ? e.subregion: 'Not Found',
-    //       area: e.area,
-    //       population: e.population,
-    //     }
-    //   });
-
-    //   for (let i = 0; i < infoApi.length; i++) {
-    //     await Country.findOrCreate({ 
-    //       where: {name: infoApi[i].name}, 
-    //       defaults: infoApi[i],
-    //     })
-    //   }
-    // }
-
   console.log(`Server listening on port ${PORT}`);
 })
 }).catch(error => console.error(error))
